62f5cfa4e077da6a7d1d8f42e4bcdf31eadb76fb
manoj-pun <manojpun840@gmail.com>
2026-02-25 08:08:29 +0545

    feat:partial post update

--- main.py
+++ main.py
@@ -193,6 +193,21 @@ def update_post_full(post_id: int, post_data: PostCreate, db: Annotated[Session,
     db.refresh(post)
     return post
 
+@app.patch("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_partial(post_id: int, post_data: PostUpdate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    update_data = post_data.model_dump(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(post, field, value)
+
+    db.commit()
+    db.refresh(post)
+    return post
+
 
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
