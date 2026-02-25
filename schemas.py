5eb66d7d8a0696bbd9d60523a8ddf4ec9ba00344
manoj-pun <manojpun840@gmail.com>
2026-02-25 08:15:38 +0545

    feat: user update delete

--- schemas.py
+++ schemas.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
 
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
 
+
 class UserCreate(UserBase):
     pass
+
 
 class UserResponse(UserBase):
     model_config = ConfigDict(from_attributes=True)
@@ -16,16 +19,26 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
+
+
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
 
+
 class PostCreate(PostBase):
-    user_id: int #Temporary
+    user_id: int  # TEMPORARY
+
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
     content: str | None = Field(default=None, min_length=1)
+
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
@@ -34,4 +47,3 @@ class PostResponse(PostBase):
     user_id: int
     date_posted: datetime
     author: UserResponse
-    
